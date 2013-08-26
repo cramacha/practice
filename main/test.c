7517b9361b5311e0312ce213febf383a3453294f
Chandra Ramachandran <chandra85@gmail.com>
2013-08-25 22:13:55 -0700

    Changing the project directory tree.

---
+++ main/test.c
@@ -0,0 +1,16 @@
+/*
+ * test.c
+ *
+ *  Created on: Aug 25, 2013
+ *      Author: chandra ramachandran
+ */
+
+#include <stdio.h>
+#include <conio>
+int main(void)
+{
+	printf("Hello world!\n");
+
+}
+
+
