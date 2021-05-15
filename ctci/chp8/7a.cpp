9a951546dbe352cb6d5563b3317711b61b5a41f6
Chandra Ramachandran <chandra85@gmail.com>
2021-05-15 15:59:09 -0700

    8.7a

---
+++ ctci/chp8/7a.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+ostream&
+operator <<(ostream &out, vector<string> &arr)
+{
+	for (int i = 0; i < arr.size(); i++)
+		out << arr[i] << endl;
+	return (out);
+}
+
+void
+permutations_impl(string input, string prefix, vector<string> &result)
+{
+	if (input.size() == 0) 
+	{ 
+		result.push_back(prefix);
+		return;
+	}
+
+	for (int i = 0; i < input.size(); i++)
+	{
+		char c = input[i];
+		string before = input.substr(0, i);
+		string after = input.substr(i + 1);
+		permutations_impl(before + after, prefix + c, result); 
+	}
+}
+
+void
+permutations(string &input)
+{
+	vector<string> result;
+	string prefix;
+	permutations_impl(input, prefix, result);
+	cout << result;
+}
+
+int main(int argc, char **argv)
+{
+	string input(argv[1]);
+
+	permutations(input);
+	return (0);
+}
