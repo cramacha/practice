dc9b8e506bc66aa914df4c5328e9df48856b6189
Chandra Ramachandran <chandra85@gmail.com>
2021-06-08 23:46:40 -0700

    qsort

---
+++ utils/qsort.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+ostream&
+operator <<(ostream &out, vector<int> &inp)
+{
+	for (int i = 0; i < inp.size(); i++)
+		out << inp[i] << " ";
+	out << endl;
+	return (out);
+}
+
+void
+qsort_impl(vector<int> &inp, int start, int end)
+{
+	int last;
+	if (start >= end)
+		return;
+	swap(inp[start], inp[(start + end)/2]);
+	last = start;
+	for (int i = start + 1; i <= end; i++)
+	{
+		if (inp[i] < inp[start])
+		{
+			last++;
+			swap(inp[i], inp[last]);
+		}
+	}
+	swap(inp[start], inp[last]);
+	qsort_impl(inp, start, last - 1);
+	qsort_impl(inp, last + 1, end);
+}
+	
+void
+qsort(vector<int> &inp)
+{
+	qsort_impl(inp, 0, inp.size() - 1);
+}
+
+
+int
+main(int argc, char **argv)
+{
+	vector<int> inp(atoi(argv[1]));
+
+	for (int i = 0; i < inp.size(); i++)
+		inp[i] = rand() % inp.size() - 1;
+	cout << inp;
+	qsort(inp);
+	cout << inp;
+	return (0);
+}
