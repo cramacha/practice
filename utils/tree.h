3db6d1c34f09ef55e857762314d6d0cec2a455a6
Chandra Ramachandran <chandra85@gmail.com>
2021-06-10 14:24:46 -0700

    Fix balancing in tree

--- utils/tree.h
+++ utils/tree.h
@@ -59,7 +59,7 @@ get_balance(node<T> *pn)
 {
 	if (pn == NULL)
 		return (0);
-	return (abs(get_height(pn->left) - get_height(pn->right)));
+	return (get_height(pn->left) - get_height(pn->right));
 }
 
 template <typename T>
@@ -91,7 +91,7 @@ void
 rotate_right(node<T> **ppn)
 {
 	node<T> *root, *left;
-	if ((root == *ppn) == NULL)
+	if ((root = *ppn) == NULL)
 		return;
 
 	left = root->left;
@@ -114,15 +114,15 @@ perform_balance(node<T> **ppn)
 	{
 		/*  Double rotation to prevent imbalance again. */
 		if (get_balance(root->right) == -1)
-			rotate_left(root->right);
-		rotate_right(root);
+			rotate_left(&root->right);
+		rotate_right(ppn);
 	}
+	/*  right heavy */
 	else if (balance == -2)
 	{
-		/*  right heavy and double rotation. */
 		if (get_balance(root->left) == 1)
-			rotate_right(root->left);
-		rotate_left(root);
+			rotate_right(&root->left);
+		rotate_left(ppn);
 	}	
 }
 
@@ -191,16 +191,16 @@ template <typename T>
 void
 tree<T>::insert_impl(node<T> **ppn, node<T> *pn)
 {
-	node<T> *root, *child;
+	node<T> *root, **child;
 
-	if ((root == *ppn) == NULL)
+	if ((root = *ppn) == NULL)
 	{
 		*ppn = pn;
 		return;
 	}
-	child = (pn->val < root->val)? root->left: root->right;
+	child = (pn->val < root->val)? &root->left: &root->right;
 	insert_impl(child, pn);
-	perform_balance(root);
+	perform_balance(ppn);
 }
 
 template <typename T>
@@ -208,7 +208,7 @@ void
 tree<T>::insert(T inp)
 {
 	node<T> *input = new node<T>(inp);
-	insert_impl(root, input);	
+	insert_impl(&root, input);	
 }
 
 #endif /*  TREE_H */
