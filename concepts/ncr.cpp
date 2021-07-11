2829a75fff12225fd91fbde681aca4bc8020200f
Chandra Ramachandran <chandra85@gmail.com>
2021-07-11 11:50:51 -0700

    Added a variation to handle duplicates - ncr problem

--- concepts/ncr.cpp
+++ concepts/ncr.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <set>
+#include <map>
 
 using namespace std;
 
@@ -27,6 +28,9 @@ void
 perm_impl(vector<int> &v, set<vector<int> > &res, int off)
 {
 	int i;
+	if (res.find(v) != res.end())
+		return;
+
 	if (off >= v.size()) {
 		res.insert(v);
 		return;
@@ -48,11 +52,56 @@ perm(vector<int> &v)
 }
 
 void
-ncr_impl(vector<int> &v, vector<int> &tmp, set<vector<int> > &res, int off, int r)
+count_nums(map<int, int> &counts, vector<int> &v)
+{
+	for (auto &elem: v) {
+		if (counts.find(elem) != counts.end())
+			counts[elem]++;
+		else
+			counts[elem] = 1;
+	}
+}
+
+void
+ncr_impl2(map<int, int> &counts, vector<int> &tmp, int off, int r)
+{
+	map<int, int>::iterator it;
+	if (tmp.size() == r) {
+		cout << tmp << endl;
+		return;
+	}
+
+	for (it = next(counts.begin(), off); it != counts.end(); it++) {
+		if (it->second > 0) {
+			it->second--;
+			tmp.push_back(it->first);
+			ncr_impl2(counts, tmp, off + 1, r);
+			it->second++;
+			tmp.pop_back();
+		}
+	}
+}
+
+void
+ncr2(vector<int> &v, int r)
+{
+	map<int, int> counts;
+	vector<int> tmp;
+	count_nums(counts, v);
+	ncr_impl2(counts, tmp, 0, r);
+}
+
+void
+ncr_impl(vector<int> &v, vector<int> &tmp,
+		set<vector<int> > &res, int off, int r)
 {
 	int i;
 
+	if (res.find(tmp) != res.end())
+		return;
+
 	if (tmp.size() == r) {
+		cout << "Inserting: " << tmp << endl;
 		res.insert(tmp);
 		return;
 	}
@@ -88,6 +137,7 @@ main(int argc, char **argv)
 	cout << "Perm" << endl;
 	perm(v);
 	cout << "ncr" << endl;
-	ncr(v, r);
+	//ncr(v, r);
+	ncr2(v, r);
 	return (0);
 }
